a3cb815fa1dd1924c23b023d9a0701b18549d6cf
Matt0105 <matteo0105@gmail.com>
2021-12-17 13:59:28 +0100

    added enter, plus button and delete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const app = new Vue(
     {
         el: "#app",
         data: {
+            newTask: "",
             todos: [
                 {
                     text: "gym",
@@ -18,7 +19,19 @@ const app = new Vue(
             ]
         },
         methods: {
+            deleteTask(index) {
+                this.todos.splice(index, 1);
+            },
 
+            addTask() {
+                let newTodo = {
+                    text: this.newTask,
+                    done: false
+                }
+
+                this.todos.unshift(newTodo);
+                this.newTask = "";
+            }
         },
         
     }
