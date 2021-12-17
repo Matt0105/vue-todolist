b4d2ddef24db72d874ccc0ae4725fd65b2fb286a
Matt0105 <matteo0105@gmail.com>
2021-12-17 13:45:45 +0100

    added line trough and v-for

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const app = new Vue(
+    {
+        el: "#app",
+        data: {
+            todos: [
+                {
+                    text: "gym",
+                    done: false
+                },
+                {
+                    text: "work",
+                    done: true
+                },
+                {
+                    text: "boolean",
+                    done: false
+                }
+            ]
+        },
+        methods: {
+
+        },
+        
+    }
+);
